6f4948797fa161cc9ec935f6b800cdce98a91833
TonySweetSQL <anthony@anthonysweetdesigns.com>
2013-11-12 15:46:15 -0500

    Changed Spacing

--- SecurityWarehouse/Powershell/SecuritydbaLib.ps1
+++ SecurityWarehouse/Powershell/SecuritydbaLib.ps1
@@ -6,50 +6,47 @@
 #  Description: Loads Powershell snap-ins, sets default Database
 #############################################################################
 
-
 # Define inventory server and database
 [String] $inventoryServer=""
 [String] $inventoryDatabase="SecurityWarehouse"
 
-
 #############################################################################
 # Add SQL Server Powershell snap-ins if they are not added yet
 #############################################################################
+
 # Load SqlServerProviderSnapin100 
 if (!(Get-PSSnapin | ?{$_.name -eq 'SqlServerProviderSnapin100'})) 
 { 
-if(Get-PSSnapin -registered | ?{$_.name -eq 'SqlServerProviderSnapin100'}) 
-{ 
-   add-pssnapin SqlServerProviderSnapin100 
-   write-host "Loading SqlServerProviderSnapin100 in session" 
+	if(Get-PSSnapin -registered | ?{$_.name -eq 'SqlServerProviderSnapin100'}) 
+	{ 
+		add-pssnapin SqlServerProviderSnapin100 
+		write-host "Loading SqlServerProviderSnapin100 in session" 
+	} 
+	else 
+	{ 
+		Get-PSSnapin
+		write-host "SqlServerProviderSnapin100 is not registered with the system." -Backgroundcolor Red –Foregroundcolor White  
+	} 
 } 
 else 
-{ Get-PSSnapin
-   write-host "SqlServerProviderSnapin100 is not registered with the system." -Backgroundcolor Red –Foregroundcolor White 
-   break 
-} 
-} 
-else 
 { 
-  write-host "SqlServerProviderSnapin100 is already loaded" 
+	write-host "SqlServerProviderSnapin100 is already loaded" 
 }  
- 
 
 # Load SqlServerCmdletSnapin100 
 if (!(Get-PSSnapin | ?{$_.name -eq 'SqlServerCmdletSnapin100'})) 
 { 
-if(Get-PSSnapin -registered | ?{$_.name -eq 'SqlServerCmdletSnapin100'}) 
-{ 
-   add-pssnapin SqlServerCmdletSnapin100 
-   write-host "Loading SqlServerCmdletSnapin100 in session" 
+	if(Get-PSSnapin -registered | ?{$_.name -eq 'SqlServerCmdletSnapin100'}) 
+	{ 
+		add-pssnapin SqlServerCmdletSnapin100 
+		write-host "Loading SqlServerCmdletSnapin100 in session" 
+	} 
+	else 
+	{ 
+		write-host "SqlServerCmdletSnapin100 is not registered with the system."  
+	} 
 } 
 else 
 { 
-   write-host "SqlServerCmdletSnapin100 is not registered with the system." 
-   break 
-} 
-} 
-else 
-{ 
-  write-host "SqlServerCmdletSnapin100 is already loaded" 
+	write-host "SqlServerCmdletSnapin100 is already loaded" 
 } 
